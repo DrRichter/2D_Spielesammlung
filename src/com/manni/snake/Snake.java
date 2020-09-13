0296ea7ebf963f1b82e1ec9e997c12299694a424
DrRichter <Kartoffed@gmail.com>
2020-09-13 18:24:14 +0200

    Score moved 5px up

--- src/com/manni/snake/Snake.java
+++ src/com/manni/snake/Snake.java
@@ -3,30 +3,24 @@ package com.manni.snake;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Snake extends JPanel implements ActionListener {
-    private int width = 300;
-    private int height = 300;
-
-    private Image apple;
-    private Image head;
-    private Image tail;
-
+    public static int direction = 1;
+    boolean running = true;
+    private final int width = 300;
+    private final int height = 300;
+    private final Image apple;
+    private final Image head;
+    private final Image tail;
     private int tail_amount = 3;
-    private int snake_witdh = 10;
-    private int snake_x[] = new int[width*height/(snake_witdh*snake_witdh)];
-    private int snake_y[] = new int[width*height/(snake_witdh*snake_witdh)];
-
+    private final int snake_witdh = 10;
+    private final int[] snake_x = new int[width * height / (snake_witdh * snake_witdh)];
+    private final int[] snake_y = new int[width * height / (snake_witdh * snake_witdh)];
     private int apple_x;
     private int apple_y;
-    boolean running = true;
-
-    public static int direction = 1;
-
-    private Timer t;
+    private final Timer t;
 
 
     public Snake() {
@@ -37,15 +31,15 @@ public class Snake extends JPanel implements ActionListener {
 
         ImageIcon icon_apple = new ImageIcon("src/com/manni/snake/resources/apple.png");
         ImageIcon icon_head = new ImageIcon("src/com/manni/snake/resources/head.png");
-        ImageIcon icon_tail =  new ImageIcon("src/com/manni/snake/resources/tail.png");
+        ImageIcon icon_tail = new ImageIcon("src/com/manni/snake/resources/tail.png");
 
         apple = icon_apple.getImage();
         head = icon_head.getImage();
         tail = icon_tail.getImage();
 
         for (int i = 0; i < tail_amount; i++) {
-            snake_x[i] = 100 - i*10;
-            snake_y[i]= 50;
+            snake_x[i] = 100 - i * 10;
+            snake_y[i] = 50;
         }
         running = true;
         t = new Timer(300, this);
@@ -54,16 +48,16 @@ public class Snake extends JPanel implements ActionListener {
     }
 
     private void spawnApple() {
-        int random = (int) (Math.random()*29);
+        int random = (int) (Math.random() * 29);
         apple_x = random * snake_witdh;
-        random = (int) (Math.random()*29);
+        random = (int) (Math.random() * 29);
         apple_y = random * snake_witdh;
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (running){
+        if (running) {
             checkApple();
             checkDeath();
             moveSnake();
@@ -73,11 +67,11 @@ public class Snake extends JPanel implements ActionListener {
 
     private void moveSnake() {
         for (int i = tail_amount; i > 0; i--) {
-            snake_x[i] = snake_x[i-1];
-            snake_y[i] = snake_y[i-1];
+            snake_x[i] = snake_x[i - 1];
+            snake_y[i] = snake_y[i - 1];
         }
 
-        switch (direction){
+        switch (direction) {
             case 0: //left
                 snake_x[0] -= snake_witdh;
                 break;
@@ -94,29 +88,28 @@ public class Snake extends JPanel implements ActionListener {
 
     private void checkDeath() {
         for (int i = tail_amount; i > 3; i--) {
-            if (snake_x[0] == snake_x[i] && snake_y[0] == snake_y[i]){
+            if (snake_x[0] == snake_x[i] && snake_y[0] == snake_y[i]) {
                 running = false;
             }
         }
 
-        if (snake_y[0] >= height || snake_x[0] >= width || snake_y[0] < 0 || snake_x[0] < 0){
+        if (snake_y[0] >= height || snake_x[0] >= width || snake_y[0] < 0 || snake_x[0] < 0) {
             running = false;
         }
-        if (!running){
+        if (!running) {
             t.stop();
         }
     }
 
     private void checkApple() {
-        if (snake_x[0] == apple_x && snake_y[0] == apple_y){
+        if (snake_x[0] == apple_x && snake_y[0] == apple_y) {
             tail_amount++;
             spawnApple();
         }
     }
 
     @Override
-    public void paintComponent(Graphics g)
-    {
+    public void paintComponent(Graphics g) {
         Font f = new Font("Calibri", Font.BOLD, 16);
         FontMetrics metrics = getFontMetrics(f);
 
@@ -124,24 +117,20 @@ public class Snake extends JPanel implements ActionListener {
         g.setFont(f);
 
         super.paintComponent(g);
-        if(running)
-        {
+        if (running) {
             g.drawImage(apple, apple_x, apple_y, this);
 
-            for(int i = 1; i < tail_amount; i++)
-            {
+            for (int i = 1; i < tail_amount; i++) {
                 g.drawImage(tail, snake_x[i], snake_y[i], this);
             }
 
             g.drawImage(head, snake_x[0], snake_y[0], this);
 
-            g.drawString("Länge = " +tail_amount, 0, height);
+            g.drawString("Länge = " + tail_amount, 0, height - 5);
 
             Toolkit.getDefaultToolkit().sync();
-        }
-        else
-        {
-            g.drawString("Game Over - Du bist gestorben!", (width - metrics.stringWidth("Game Over - Du bist gestorben!           ")), height/2);
+        } else {
+            g.drawString("Game Over - Du bist gestorben!", (width - metrics.stringWidth("Game Over - Du bist gestorben!           ")), height / 2);
         }
     }
 }
